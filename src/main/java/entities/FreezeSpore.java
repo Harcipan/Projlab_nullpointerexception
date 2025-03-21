ef82735ac4378b37b0cbd9996dc84102aac68673
Tóth Mihály Balázs <toth.misi05@gmail.com>
2025-03-21 20:12:18 +0100

    added comments

--- src/main/java/entities/FreezeSpore.java
+++ src/main/java/entities/FreezeSpore.java
@@ -5,12 +5,19 @@ public class FreezeSpore extends Spore {
         super(nutrientValue, lifetime, effectTime, effectValue);
     }
 
+    /*
+     * Set insect speed to -100, effectively disabling movement
+     */
     @Override
     public void getEaten(Insect i) {
+        isConsumed = true;
         i.setSpeedPercent(-100);
         i.addSpore(this);
     }
 
+    /*
+     * Restore insect speed to 0 (applies default speed)
+     */
     @Override
     public void removeEffect(Insect i) {
         i.setSpeedPercent(0);
--- src/main/java/entities/SpeedUpSpore.java
+++ src/main/java/entities/SpeedUpSpore.java
@@ -1,16 +1,27 @@
 package entities;
 
+/*
+ * Spore that speeds up the insect
+ * effectValue must be greater than 100 (its a percentage)
+ */
 public class SpeedUpSpore extends Spore {
     public SpeedUpSpore(int nutrientValue, int lifetime, int effectTime, int effectValue) {
         super(nutrientValue, lifetime, effectTime, effectValue);
     }
 
+    /*
+     * apply speed increase to the insect
+     */
     @Override
     public void getEaten(Insect i) {
-        i.setSpeedPercent(efffectValue);
+        isConsumed = true;
+        i.setSpeedPercent(effectValue);
         i.addSpore(this);
     }
 
+    /*
+     * Restore insect speed to 0 (applies default speed)
+     */
     @Override
     public void removeEffect(Insect i) {
         i.setSpeedPercent(0);

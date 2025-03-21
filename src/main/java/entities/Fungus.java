98fb41d120126474c93bde0e15fa472bbe5657ab
Tóth Mihály Balázs <toth.misi05@gmail.com>
2025-03-21 14:27:31 +0100

    some basic classes, very WIP

---
+++ src/main/java/entities/Fungus.java
@@ -0,0 +1,30 @@
+package entities;
+
+public abstract class Fungus extends GameEntity {
+    int health;
+
+    protected Fungus(int health, Tile currentTile) {
+        super(currentTile);
+        this.health = health;
+    }
+
+    public void growMycelium(Tile neighbor) {
+        boolean isValid = checkNeighbor(neighbor);
+        // Will implement later
+    }
+
+    public void growBody(Tile neighbor) {
+        boolean isValid = checkNeighbor(neighbor);
+        // Will implement later
+    }
+
+    public void die() {
+        // Will implement later
+    }
+
+
+    private boolean checkNeighbor(Tile neighbor) {
+        // Will implement later
+        return false;
+    }
+}

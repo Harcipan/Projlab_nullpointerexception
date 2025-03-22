a4e0a81aa3f0c2a3c2412991d55848605d9bc217
Gyárfás Réka <gyarfas.reka@gmail.com>
2025-03-22 20:36:23 +0100

    fixed code that got broken by new constructos

--- src/main/java/use_cases/InsectMove.java
+++ src/main/java/use_cases/InsectMove.java
@@ -15,21 +15,28 @@ public class InsectMove extends UseCase {
     public void execute() {
         //insect Player is created
         InsectPlayer ip=new InsectPlayer();
+        printWrapper("InsectPlayer: i",Direction.LEFT);
 
         //tekton of tiles
         Tekton tek=new Tekton(1,1);
+        printWrapper("Tekton: tek",Direction.LEFT);
+
 
         //current tile cration
         Tile currentTile=new Tile();
+        printWrapper("Tile: currentTile",Direction.LEFT);
         
         //target tile creation
         Tile targetTile=new Tile();
+        printWrapper("Tile: targetTile",Direction.LEFT);
 
         //insect creation
         Insect i=new Insect();
+        printWrapper("Insect: i",Direction.LEFT);
 
         //add tiles to tekton
         currentTile.setParentTekton(tek);
+        printWrapper("currentTile.setParentTekton()",Direction.LEFT);
         targetTile.setParentTekton(tek);
 
         
@@ -38,5 +45,9 @@ public class InsectMove extends UseCase {
 
         //add insect to insectplayer
         ip.setControlledInsect(i);
+
+
+        //insect steps
+        ip.moveTo(targetTile);
     }
 }

ea5981860b688e110b8bdbe0339d1430ae89fd56
forianzsiga <forianzsiga@gmail.com>
2025-03-22 12:08:48 +0100

    added FungusGrowingAMushroom Use-Case modified FungusPlayer.java, Map.java and MyceliumDies.java to accomodate new features needed for testing (not full implementation, just placeholder comments, iterations and printing)

--- src/main/java/use_cases/MyceliumDies.java
+++ src/main/java/use_cases/MyceliumDies.java
@@ -1,5 +1,7 @@
 package use_cases;
 
+import entities.Mycelium;
+import map.*;
 
 public class MyceliumDies extends UseCase {
 
@@ -7,8 +9,41 @@ public class MyceliumDies extends UseCase {
         super(1, "Mycelium Dies");
     }
 
+    @Override
     public void execute() {
-        System.out.println("Mycelium dies.");
+
+        //scene initialization
+        System.out.println("Initializing scene...");
+
+        // map creation
+        System.out.println("Creating map...");
+        Map m = new Map();
+
+        // tekton creation
+        System.out.println("Creating tekton...");
+        Tekton tek = new Tekton(1, 1);
+
+        // add tekton to map
+        m.addTekton(tek);
+
+        // tile creation and parent tekton assignment in constructor
+        System.out.println("Creating tile...");
+        Tile t = new Tile(1, 1, tek);
+
+        // mycelium creation and tile assignment in constructor
+        System.out.println("Creating mycelium...");
+        Mycelium myc = new Mycelium(1, t);
+
+        // make the reference mutual
+        t.addEntity(myc);
+
+        // action: mycelium dies
+        System.out.println("Mycelium dies...");
+
+        // remove mycelium from tile
+        t.removeEntity(myc);
+
+        m.tick();  // this will call the mycelium's detach method (connectivity determined here)
     }
 }
 

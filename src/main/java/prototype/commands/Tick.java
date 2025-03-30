7eeadaf17d01ed71e3db9ad722144d8668d61670
Kuzmin Iván Georgijevics <cimalolnak@gmail.com>
2025-03-31 01:36:49 +0200

    Implement `tick` command

--- src/main/java/prototype/commands/Tick.java
+++ src/main/java/prototype/commands/Tick.java
@@ -8,6 +8,23 @@ public class Tick extends Command {
 
     @Override
     public boolean execute(String[] args) {
-        throw new UnsupportedOperationException("not implemented");
+        if(isWrongNumberOfArgs(2, args.length)) return false;
+        if(isMapUninitialized()) return false;
+
+        int rounds;
+        try {
+            rounds = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            System.out.println("This is not a number. The number of rounds can only be a number.");
+            return false;
+        }
+        if (rounds < 0) {
+            System.out.println("The number of rounds must be positive");
+            return false;
+        }
+
+        for(int i = 0; i < rounds; i++){
+            app.getMap().tick();
+        }
     }
 }
